cbcef5fad0527b68db6b33fe46694487db7612c4
ArionildoEscouto <arionildo6@gmail.com>
2020-04-18 19:08:29 -0300

    Add Script Bonfire and Refactory Room

---
+++ src/Assets/Scripts/Actor/Bonfire.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bonfire : MonoBehaviour
+{
+    public string tag = "Player";
+    private SpriteRenderer spriteRenderer;
+    private float fireIntensity = 1f;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetMouseButtonDown(0)
+            && other.CompareTag(tag)
+            && fireIntensity > 0)
+        {
+            fireIntensity -= .3f;
+            spriteRenderer.color = new Color(1f, fireIntensity, fireIntensity, 1f);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        fireIntensity = 1;
+        spriteRenderer.color = new Color(1f, .3f, .3f, 1f);
+    }
+}
