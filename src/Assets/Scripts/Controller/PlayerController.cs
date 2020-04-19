0a01e5968bf18f7a016efc120de4630f2471f8e9
Matheus Ligabue <matheus.s.ligabue@gmail.com>
2020-04-19 18:30:50 -0300

    Add player sprites and animation

--- src/Assets/Scripts/Controller/PlayerController.cs
+++ src/Assets/Scripts/Controller/PlayerController.cs
@@ -4,34 +4,52 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed;
-    private float moveHorizontal;
-    private float moveVertical;
+    private Vector2 direction;
+    private Animator animator;
+
+    private SpriteRenderer spriteRenderer;
+
+    private Rigidbody2D rb2d;
 
     void Start()
     {
         Time.timeScale = 1;
         PlayerManager.instance.alive = true;
+        animator = gameObject.GetComponent<Animator>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        moveHorizontal = Input.GetAxis("Horizontal");
-        moveVertical = Input.GetAxis("Vertical");
+        direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        Vector2 direction = new Vector2(moveHorizontal, moveVertical);
-        Vector2 translate = direction * Time.deltaTime * (speed * PlayerManager.gameSpeed);
-        
-        transform.Translate(translate);
+        if (direction.magnitude > 0f)
+        {
+            animator.Play("Sprint");
+        }
+        else
+        {
+            animator.Play("Idle");
+        }
+
+        if (direction.x < 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        if (direction.x > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        Vector2 force = direction * speed;
+        rb2d.AddForce(force);
     }
 
     public void Die()
     {
         PlayerManager.instance.alive = false;
         Time.timeScale = 0;
-    }
-
-    public void Stop()
-    {
-        moveHorizontal = 0;
     }
 }
