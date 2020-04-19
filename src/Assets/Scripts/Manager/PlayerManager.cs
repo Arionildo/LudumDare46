e54a35fd4112627be2b2574fcdd1f82a00a3c001
Liliane Castro <lcs2@icomp.ufam.edu.br>
2020-04-19 02:09:41 -0400

    wip: HUD, sprite: progressLife and Fire

--- src/Assets/Scripts/Manager/PlayerManager.cs
+++ src/Assets/Scripts/Manager/PlayerManager.cs
@@ -2,14 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager: MonoBehaviour
 {
     public static PlayerManager instance = null;
+    public Slider progressLife;
+
     public static float gameSpeed;
     public static int score;
 
     public bool alive = false;
+
+    public float targetProgressLife;
 
     void Awake()
     {
@@ -17,13 +22,33 @@ public class PlayerManager: MonoBehaviour
             instance = this;
         else if (instance != this)
             Destroy(gameObject);
-    
+
         DontDestroyOnLoad(gameObject);
         gameSpeed = 1f;
+    }
+
+    private void Update() {
+        ProgressLife();
     }
 
     internal static void ResetValues()
     {
         score = 0;
     }
+
+
+    private void DecrementProgress(float newProgress)
+    {
+        targetProgressLife = progressLife.value - newProgress;
+    }
+
+    void ProgressLife()
+    {
+        if (progressLife.value > targetProgressLife)
+        {
+            progressLife.value -= Time.deltaTime;
+        }
+        DecrementProgress(0.75f);
+    }
+
 }
