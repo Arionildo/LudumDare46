61ca23dfccefbb97bd5639aa59f775e48a431e7e
ArionildoEscouto <arionildo6@gmail.com>
2020-04-19 01:56:17 -0300

    Add Script Collectable and Fix Collision Methods

--- src/Assets/Scripts/Actor/Bonfire.cs
+++ src/Assets/Scripts/Actor/Bonfire.cs
@@ -4,28 +4,35 @@ using UnityEngine;
 
 public class Bonfire : MonoBehaviour
 {
-    public new string tag = "Player";
+    public new string playerTag = "Player";
     private SpriteRenderer spriteRenderer;
     private float fireIntensity = 1f;
+    private bool isAvailable = false;
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    void OnTriggerStay2D(Collider2D other)
+    void Update()
     {
-        if (Input.GetMouseButtonDown(0)
-            && other.CompareTag(tag)
-            && fireIntensity > 0)
+        if (isAvailable
+            && fireIntensity > 0
+            && Input.GetMouseButtonDown(0))
         {
             fireIntensity -= .3f;
             spriteRenderer.color = new Color(1f, fireIntensity, fireIntensity, 1f);
         }
     }
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        isAvailable = other.CompareTag(playerTag) && fireIntensity > 0;
+    }
+
     void OnTriggerExit2D(Collider2D other)
     {
+        isAvailable = !other.CompareTag(playerTag);
         fireIntensity = 1;
         spriteRenderer.color = new Color(1f, .3f, .3f, 1f);
     }
