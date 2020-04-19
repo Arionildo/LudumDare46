14993d894e4a075e48ff20b2fc0f0c4162880edf
andriuswill <awmdsilva@outlook.com>
2020-04-18 23:14:01 -0300

    logica de transicao nao esta funcionando

--- src/Assets/Scripts/Actor/Bonfire.cs
+++ src/Assets/Scripts/Actor/Bonfire.cs
@@ -6,11 +6,14 @@ public class Bonfire : MonoBehaviour
 {
     public new string tag = "Player";
     private SpriteRenderer spriteRenderer;
+    private Animator animator;
     private float fireIntensity = 1f;
+    private float lifeTime = 10f;
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     void OnTriggerStay2D(Collider2D other)
@@ -29,4 +32,27 @@ public class Bonfire : MonoBehaviour
         fireIntensity = 1;
         spriteRenderer.color = new Color(1f, .3f, .3f, 1f);
     }
+
+    void Update() {
+        lifeTime -= Time.deltaTime;    
+        Debug.Log(lifeTime);
+        if(lifeTime < 7){
+            animator.SetBool("isMedium", true);
+        } else {
+            animator.SetBool("isMedium", false);     
+        } 
+    }
+
+    void setLargeBonfire(){
+
+    }
+
+    void setMediumBonfire(){
+        
+    }
+
+    void setSmallBonfire(){
+        
+    }
+
 }
